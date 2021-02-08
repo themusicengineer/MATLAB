f156aff24f5a964050e6c86a6f74e2b78180e3e2
Jonathan Tran <jonathan.t.a.tran@gmail.com>
2021-02-08 14:01:41 -0800

    HW2 upload
    HW2 from EE210

---
+++ EE210_HW2_c.m
@@ -0,0 +1,38 @@
+clc;
+clear;
+
+syms t f
+
+x1 = 1;
+x1 = x1*exp(-j*2*pi*f*t);
+
+x2 = 1/2*t + 3;
+x2 = x2*exp(-j*2*pi*f*t);
+
+X1 = int(x1,t,-4,-3); % integrate
+
+X2 = int(x2,t,-6,-4); % integrate
+
+X = X1 + X2
+
+% pretty(simplify(x2));
+
+f = -5:0.011:5; %
+
+% X1 = subs(X1,f); % substitution of frequency in X_f
+% X2 = subs(X2,f); % substitution of frequency in X_f
+% 
+% X1 = double(X1); % type cast from symbol type to double
+% X2 = double(X2); % type cast from symbol type to double
+% 
+% subplot(4,1,1), plot(f,abs(abs(X1))); % plot magnitude
+% subplot(4,1,2), plot(f,angle(X1)); % plot phase
+% 
+% subplot(4,1,3), plot(f,abs(abs(X2))); % plot magnitude
+% subplot(4,1,4), plot(f,angle(X2)); % plot phase
+
+X = subs(X,f); % substitution of frequency in X_f
+X = double(X); % type cast from symbol type to double
+
+subplot(2,1,1), plot(f,abs(abs(X))); % plot magnitude
+subplot(2,1,2), plot(f,angle(X)); % plot phase
